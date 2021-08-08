de01c79ae517ac6dda6eafa5d8212ca26234b250
GitHub Action <action@github.com>
2021-08-08 09:48:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/meimingle/tsvnpwdintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/meimingle/tsvnpwdintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.meimingle.tsvnpwdintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.meimingle.tsvnpwdintellij.MyBundle
 
 class MyApplicationService {
 
